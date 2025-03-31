b339d8a80bea83d3c4aba43e731d7a2e935c36fd
jingjiezhuang <jingjie.zhuang@igg.com>
2025-03-31 19:35:03 +0800

    tango: Tango support for waydroid
    Signed-off-by: jingjiezhuang <jingjie.zhuang@igg.com>

--- device.mk
+++ device.mk
@@ -234,3 +234,5 @@ ifeq ($(filter %_waydroid_x86 %_waydroid_x86_64,$(TARGET_PRODUCT)),)
 PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30
 endif
 
+TANGO_PRETRANSLATE_WHITELIST := device/waydroid/waydroid/oriole-tango-whitelist.txt
+include $(wildcard vendor/amanieu/tango/tango.mk)
